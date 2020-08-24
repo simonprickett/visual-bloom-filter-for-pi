70c2343b946dfc55aa43b195ef54ec138096dae2
Simon Prickett <simon@crudworks.org>
2020-08-23 17:14:02 -0700

    First real go at styling.

--- static/app.js
+++ static/app.js
@@ -1,1 +1,14 @@
-/* TODO */
+  window.onload = function () {
+    const addButton = document.getElementById('addButton');
+    const existsButton = document.getElementById('existsButton');
+
+    addButton.onclick = async function (e) {
+      alert('add');
+      e.preventDefault();
+    }
+
+    existsButton.onclick = async function (e) {
+      e.preventDefault();
+      alert('exists!');
+    }
+  };
