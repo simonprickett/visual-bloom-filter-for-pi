8dd5f06eaa821f841ae33ee19cd2bfae2247dc8a
Simon Prickett <simon@crudworks.org>
2020-08-23 18:45:54 -0700

    Further work on calling backend.

--- static/app.js
+++ static/app.js
@@ -1,10 +1,24 @@
   window.onload = function () {
+    const API_PREFIX = 'api';
+
+    const getResult = async function(response) {
+      const responseData = await response.json();
+      return responseData.result;
+    };
+
     const callBloomFilter = async function (element, isAdd) {
-      // TODO call the filter...
-      const result = true;
+      // TODO POST if it is the exists button...
+      const response = await fetch(`/${API_PREFIX}/${isAdd ? 'add' : 'exists'}/${element}`);
+
+      if (response.status !== 200  && response.status !== 201) {
+        document.getElementById('displayResult').innerHTML = `Error calling backend :(`;
+        return;
+      }
+
+      const result = await getResult(response);
 
       if (isAdd) {
-        document.getElementById('displayResult').innerHTML = `<strong>${element}</strong> was added to the Bloom filter.`;
+        document.getElementById('displayResult').innerHTML = `<strong>${element}</strong> was ${result ? '' : 'not '}added to the Bloom filter.`;
       } else {
         document.getElementById('displayResult').innerHTML = `<strong>${element}</strong> ${result ? 'may be' : 'is not'} in the Bloom filter.`;
       }
@@ -34,6 +48,8 @@
 
     document.getElementById('resetButton').onclick = async function (e) {
       e.preventDefault();
+
+      // TODO POST to the backend...
 
       document.getElementById('displayResult').innerHTML = 'Filter reset.';
       document.getElementById('elementText').value = '';
