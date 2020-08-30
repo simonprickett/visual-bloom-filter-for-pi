c8c43d4f98788350163a2ebaebbf04c892aa66ea
Simon Prickett <simon@crudworks.org>
2020-08-29 17:55:50 -0700

    Basically working needs to show setting LEDs in parallel.

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import unicornhat
 API_PATH_PREFIX = 'api'
 NUM_HASH_FUNCTIONS = 3
 NUM_LEDS = 64
-COLOR_BIT_UNSET = 'TODO'
-COLOR_BIT_SET = 'TODO'
-COLOR_BIT_QUERY = 'TODO'
+COLOR_BIT_SET = (255, 0, 0)
+COLOR_BIT_WRITING = (255, 165, 0)
+COLOR_BIT_QUERYING = (65, 105, 225) 
 
 app = Flask(__name__)
 
@@ -26,18 +26,35 @@ def get_led_position(led):
     unicorn_width, unicorn_height = unicornhat.get_shape()
     return (led % unicorn_height, led // unicorn_width)
 
+def toggle_led(pos, transition_color, new_color):
+    # This should take a list of tuples for all 3 pixels to change...
+    # This should also have an interim color to use
+
+    orig_color = unicornhat.get_pixel(pos[0], pos[1])
+
+    for n in range(4):
+        unicornhat.set_pixel(pos[0], pos[1], transition_color[0], transition_color[1], transition_color[2])
+        unicornhat.show()
+        time.sleep(0.3)
+        unicornhat.set_pixel(pos[0], pos[1], orig_color[0], orig_color[1], orig_color[2])
+        unicornhat.show()
+        time.sleep(0.3)
+
+        if n == 3:
+            unicornhat.set_pixel(pos[0], pos[1], new_color[0], new_color[1], new_color[2])
+            unicornhat.show()
+
 def query_led_status(led):
     pos = get_led_position(led)
-    # TODO some animation
+
     r, g, b = unicornhat.get_pixel(pos[0], pos[1])
+    toggle_led(pos, COLOR_BIT_QUERYING, (r, g, b))
+
     return not (r == 0 and g == 0 and b == 0) 
 
 def set_led_status(led):
-    # TODO animate this...
     pos = get_led_position(led)
-
-    unicornhat.set_pixel(pos[0], pos[1], 255, 0, 0)
-    unicornhat.show()
+    toggle_led(pos, COLOR_BIT_WRITING, COLOR_BIT_SET)
 
 def add_to_filter(element):
     for n in range(NUM_HASH_FUNCTIONS): 
